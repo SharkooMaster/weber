29f272b05ec3569c1aac748bcf6c6365efd4c73f
Ali Al Rashini <44705253+SharkooMaster@users.noreply.github.com>
2023-03-21 15:42:44 +0100

    Add files via upload

---
+++ compile.py
@@ -0,0 +1,86 @@
+import json
+import glob
+from bs4 import BeautifulSoup as bs
+
+class compiler:
+    configPath = "./config.json"
+    config = None
+    
+    componentFilePaths = []
+    componentFiles = []
+    
+    pageFiles = []
+    compiledPages = []
+    
+    def __init__(self):
+        self.config = json.loads(open(self.configPath, "r").read())
+        self.componentFilePaths = self.getFromDir(self.config["html"], self.config["gateway"])
+        self.componentFiles = self.getFiles(self.componentFilePaths)
+        print(self.componentFilePaths)
+        print(self.componentFiles)
+    
+        self.pageFiles = self.getFiles(self.config["gateway"])
+        print(self.pageFiles)
+        self.compiledPages = self.parseFiles_html(self.pageFiles)
+        self.build()
+    
+    def getFromDir(self, p, m):
+        ret = []
+        for i in range(len(p)):
+            for files in glob.glob(f"{p[i]}/*.html"):
+                if files not in m:
+                    ret.append(files)
+        return ret
+    
+    def getFiles(self, p):
+        ret = []
+        for i in p:
+            ret.append(open(i,"r").read())
+        return ret
+    
+    def parseFiles_html(self, p):
+        compiledPageFiles = []
+        for i in p:
+            while True:
+                if(i.find("<?") == -1):
+                    compiledPageFiles.append(i)
+                    break
+                start = i.find("<?")
+                end = i.find("?>")
+    
+                component = i[start:end]
+                print(component)
+                compArgs = component.split(" ")[1:]
+                print(compArgs)
+    
+                compName = compArgs[0]
+                print(compName)
+    
+                compFile = ""
+                for j in range(len(self.componentFilePaths)):
+                    if(compName in self.componentFilePaths[j]):
+                        compFile = self.componentFiles[j]
+                        argsLen = len(compArgs) - 1
+                        for k in compArgs[1:]:
+                            if(k != ''):
+                                _tag = k.split("=")[0]
+                                _val = k.split("=")[1]
+                                print(_tag)
+                                if("{" + _tag + "}" in compFile):
+                                    compFile = compFile.replace("{" + _tag + "}", _val)
+                        break
+                i = i[:start] + compFile + i[end + 2:]
+
+                for x in self.config["vars"]:
+                    if "{" + x + "}" in i:
+                        i = i.replace("{"+x+"}",self.config["vars"][x])
+                i = bs(i, features="html.parser").prettify()
+        print(compiledPageFiles)
+        return compiledPageFiles
+    
+    def build(self):
+        for i in range(len(self.config["gateway"])):
+            _n = self.config["gateway"][i].split("/")
+            with open(f'{self.config["buildPath"]}/{_n[len(_n)-1]}', "w") as f:
+                f.write(self.compiledPages[i])
+x = compiler()
