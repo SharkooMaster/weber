43f3c56e9d12478e8f29fdd9af3b83ee4f82774a
Ali Al Rashini <44705253+SharkooMaster@users.noreply.github.com>
2023-03-23 18:25:49 +0100

    Single page updates

--- build/index.js
+++ build/index.js
@@ -16,17 +16,46 @@ function mobNav()
 	}
 }
 
+let activeIndex = 0;
+
+addEventListener("popstate", (event)=>{
+	for (let index = 0; index < paths.length; index++) {
+		document.getElementById(paths[index]).style.display = "none";
+		if(paths[index] === window.history.state["id"])
+		{
+			activeIndex = index
+			document.getElementById(paths[index]).style.display = "block";
+		}
+	}
+})
+
+window.onbeforeunload = (e)=>
+{
+	if(e)
+	{
+		e.returnValue = "leaving"
+	}
+}
+
+window.onunload = (e)=>
+{
+	console.log('test')
+	window.location = "http://127.0.0.1:5500/build?id="+activeIndex
+}
+
 function navPage(a)
 {
+	activeIndex = a;
 	for (let index = 0; index < paths.length; index++) {
+		console.log(index)
 		if(a == index){
 			document.getElementById(paths[index]).style.display = "block";
-			window.history.pushState({id:"100"}, "Page", "/"+paths[index])
+			window.history.pushState({id: paths[index]}, paths[index], "/"+paths[index])
 		}
 		else{
 			document.getElementById(paths[index]).style.display = "none";
 		}
 	}
-	navBarView_mobile.style.display = "none"
+	
 }
 navPage(0)
